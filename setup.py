031efb2d95937f1e4590f04c33c32bdffbe5bbaf
inerttila <inert.etila@gmail.com>
2024-01-31 16:54:03 +0100

    add

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inert',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         # List your package dependencies here
